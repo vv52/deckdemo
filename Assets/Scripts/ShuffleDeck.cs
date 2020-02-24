472d020715f7a29b364bedfef7056cd77c56256a
Vanja <vv52@humboldt.edu>
2020-02-23 16:39:15 -0800

    Added "Shuffle Deck" button that simply re-triggers the already implemented Shuffle() function, complete with Debug.Log() output.

---
+++ Assets/Scripts/ShuffleDeck.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuffleDeck : MonoBehaviour
+{
+	public GameManager gameManager;
+
+    public void OnClick()
+    {
+    	gameManager.ShuffleDeck();
+    }
+}
